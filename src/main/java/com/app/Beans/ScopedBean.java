4b97d0d23e179046e50d9d0b2848ec73c4810629
SylverVB <victorelish@gmail.com>
2025-04-16 22:01:21 -0400

    Add basic ScopedBean class as a simple Spring bean example
    - Introduced a plain Java class to serve as a bean in the Spring ApplicationContext.
    - This bean will be used to demonstrate different bean scopes in Spring (singleton vs prototype).
    - No methods or properties are defined; this is a structural bean for testing purposes.

---
+++ src/main/java/com/app/Beans/ScopedBean.java
@@ -0,0 +1,8 @@
+package com.app.Beans;
+
+/**
+ * This class will be used in two different Beans, defined in the class marked @Configuration (Application.class).
+ */
+public class ScopedBean {
+
+}
